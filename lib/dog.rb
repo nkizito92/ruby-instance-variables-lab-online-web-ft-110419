3638c5353ffa8b403670ea96567eb44e39deb9a4
Kizito Njoku <nkizito57@gmail.com>
2019-11-11 17:39:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   this_dogs_name
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    this_dogs_name = dogs_name
   end
   
   def name 
-    @this_dogs_name
+    this_dogs_name
   end 
 end
